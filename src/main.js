918a131163da421b513f38657ff4d61a350b9a25
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-03-31 19:25:43 +0300

    router updated

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from './pages/landing_page/landing_page'
+import FirstAidPage from "./pages/first_aid_page/first_aid_page";
 import './main.css'
 
 function Main(){
     return(
         <Routes>
             <Route path ='/' element = {<LandingPage/>}/>
+            <Route path ='/firstaidkit' element = {<FirstAidPage/>}/>
         </Routes>
     );
 }
