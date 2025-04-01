b7a7e48aae2d9adcc41d2cf64842a850e6a1df3a
Ija Maharyta <ijamaharyta@gmail.com>
2025-04-01 17:02:42 +0300

    fix: fixed import from file userService

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -2,7 +2,6 @@ const registerUser = async (userData) => {
   // method for sending data to the server
   try {
     const response = await fetch("http://localhost:3001/api/auth/register", {
-      mode: "no-cors",
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(userData),
@@ -15,11 +14,4 @@ const registerUser = async (userData) => {
   }
 };
 
-// const handleSubmit = async (e) => {
-//   e.preventDefault();
-//   const userData = { username, email, password };
-//   const result = await registerUser(userData);
-//   console.log(result);
-// };
-
-// export { handleSubmit };
+export { registerUser };
