cec59d4c955770459d6ceedf479562a373a9d5e1
Ija Maharyta <ijamaharyta@gmail.com>
2025-04-08 17:50:44 +0300

    feat(main.js): added route to registration page

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from './pages/landing_page/landing_page'
 import FirstAidPage from "./pages/first_aid_page/first_aid_page";
+import RegistrationForm from "./pages/registartion_form/RegisterForm";
 import './main.css'
 
 function Main(){
-    return(
-        <Routes>
-            <Route path ='/' element = {<LandingPage/>}/>
-            <Route path ='/firstaidkit' element = {<FirstAidPage/>}/>
-        </Routes>
+    return (
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/firstaidkit" element={<FirstAidPage />} />
+        <Route path="/registration" element={<RegistrationForm />} />
+      </Routes>
     );
 }
 
