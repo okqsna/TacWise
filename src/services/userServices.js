a84a41c70c1c4d35cc8db3fabd01c4eccaac3d91
Ija Maharyta <ijamaharyta@gmail.com>
2025-04-08 18:54:42 +0300

    feat(userServices.js): added LoginUser func

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -18,10 +18,25 @@ const registerUser = async (userData) => {
     console.error("Error:", error);
   }
 };
+
+const loginUser = async (userData) => {
+  // method for sending data to the server
+  try {
+    const response = await fetch("http://localhost:3001/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    });
+
+    const data = await response.json();
+    if (data.token) {
+      sessionStorage.setItem("token", data.token);
+      window.location.href = "/"; // redirect to home page
+    }
     return data;
   } catch (error) {
     console.error("Error:", error);
   }
 };
 
-export { registerUser };
+export { registerUser, loginUser };
