1f34818d9ec716c1f664cf97a6f3ba954c6426f3
Ija Maharyta <ijamaharyta@gmail.com>
2025-04-08 17:51:32 +0300

    refactor(RegisterForm.jsx): corrected path

--- src/pages/registartion_form/RegisterForm.jsx
+++ src/pages/registartion_form/RegisterForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './RegisterForm.scss';
-import RegisterForm from '../../components/registartion_form/RegisterForm';
+import RegisterForm from '../../components/registration_form/RegisterForm';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 
