14289ddaae8d61e3389e384fec5f8d1cf2bd9ca6
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-08 23:09:11 +0300

    routes fixed

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from './pages/landing_page/landing_page'
 import FirstAidPage from "./pages/first_aid_page/first_aid_page";
-import RegistrationForm from "./pages/registartion_form/RegisterPage";
-import LoginPage from "./pages/log_in_page/login_page";
+import RegisterPage from './pages/register_page/RegisterPage.jsx';
+import LoginPage from "./pages/login_page/login_page.jsx";
 import './main.css'
 
 function Main(){
     return (
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/firstaidkit" element={<FirstAidPage />} />
-        <Route path="/registration" element={<RegistrationForm />} />
+        <Route path="/firstaidkit" element={<FirstAidPage/>} />
+        <Route path="/registration" element={<RegisterPage/>} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     );
