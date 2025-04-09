867dc87057e368a5c1f58eef153c5ec0d6df1388
oToTak <t.teodorko@gmail.com>
2025-04-09 21:33:03 +0300

    Added page files

--- src/pages/test_cards_page/test_cards_page.jsx
+++ src/pages/test_cards_page/test_cards_page.jsx
@@ -1,1 +1,21 @@
-// cringe
+import React from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import Stuname from "../../components/stuname/stuname";
+import "./test_cards.scss";
+
+const TestCardsPage = () => {
+  return (
+    <div className="test-cards-page">
+      <div className="test-cards-page-header">
+        <Header />
+      </div>
+      <main className="content">
+        <Stuname />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default TestCardsPage;
--- src/pages/test_cards_tf_page/test_cards_tf_page.jsx
+++ src/pages/test_cards_tf_page/test_cards_tf_page.jsx
@@ -1,1 +1,21 @@
-// cringe
+import React from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import Stuname from "../../components/stuname/stuname";
+import "./test_card_tf_page.scss";
+
+const TestCardTfPage = () => {
+  return (
+    <div className="test-card-tf-page">
+      <div className="test-card-tf-page-header">
+        <Header />
+      </div>
+      <main className="content">
+        <Stuname />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default TestCardTfPage;
