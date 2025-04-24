52428db7f317e357fb73c1d512e80d080228d43e
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-25 00:37:34 +0300

    fix(login): bug wrong user info fixed

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -4,11 +4,11 @@ import './login.scss'
 import { loginUser } from '../../services/userServices.js'; 
 
 function Login() {
-  const [formData, setFormData] = useState({ // initial state for form data
-         email: '',
-         password: ''
-     });
-  
+    const [formData, setFormData] = useState({ // initial state for form data
+        email: '',
+        password: ''
+    });
+    const [error, setError] = useState(null);
  
      const handleChange = (e) => { // function to handle input changes
        e.preventDefault();
@@ -18,11 +18,33 @@ function Login() {
         });
      };
  
-     const handleSubmit = (e) => {
-       e.preventDefault();
-       console.log(formData);
-         loginUser(formData)
-     }
+     const handleSubmit = async (e) => {
+      e.preventDefault();
+    
+      try {
+        const response = await loginUser(formData);
+    
+        if (response.error) {
+          let errorMessage = response.error;
+
+          if (errorMessage === 'User does not exist') {
+            errorMessage = 'Користувача не існує 🙉';
+          } else if (errorMessage === 'Invalid password') {
+            errorMessage = 'Неправильний пароль ⚒️';
+          }
+        
+          setError(errorMessage);
+          return;
+        }
+    
+        setError(null);
+        console.log('Login success:', response);
+      } catch (err) {
+        console.error('Login failed:', err);
+        setError('Щось пішло не так. Спробуйте ще раз.');
+      }
+    };
+    
   return (
     <div className="loginContainer">
       <h1 className='loginContainer_cap'>Увійдіть, і нумо навчатись!</h1>
@@ -31,6 +53,7 @@ function Login() {
         <input type="password" placeholder='Пароль' name='password' id='password' className='loginContainer_form_input'/>
         <a href="/registration" className='loginContainer_link'>Або створіть акаунт</a>
         <button type='submit' className='loginContainer_btn'>Увійти</button>
+        <p className='loginContainer_error'>{error}</p>
       </form>
     </div>
   );
